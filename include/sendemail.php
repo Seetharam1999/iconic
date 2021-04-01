9fd76468fa0698ebfac05b7abdaa777e65d30326
SR Seetharam <38151089+Seetharam1999@users.noreply.github.com>
2021-04-01 15:50:51 +0530

    updating email access

---
+++ include/sendemail.php
@@ -0,0 +1,38 @@
+ <?php
+ 
+require __DIR__.'/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
+ $userEmail = ""; 
+   
+   $client=new Client();
+   
+   $headers = [ 'authorization' => 'Bearer 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',
+        'content-type'     => 'application/json'];
+   $body='';
+   $bodyadmin='';
+   $request='';
+     $request1='';
+   if(isset($_POST['subscribe'])){ 
+         $subject = "ICONIC DREAM FOCUS PVT LTD - Thanks for subscribing us";
+         $message = "Thank You for joining your hands with us. You'll always receive updates from us. And we won't share and sell your information.";
+        $body = '{"personalizations":[{"to":[{"email":"'.$_POST['email'].'","name":"ICONIC DREAM FOCUS"}],"subject":"'.$subject.'"}],"content": [{"type": "text/plain", "value":"'.$message.'"}],"from":{"email":"idficonic@gmail.com","name":"Iconic Dream Focus"}}';
+  $bodyadmin = '{"personalizations":[{"to":[{"email":"srseetharam1999@gmail.com","name":"ICONIC DREAM FOCUS"}],"subject":"subscriber list "}],"content": [{"type": "text/plain", "value":"mail:'.$_POST['email'].'"}],"from":{"email":"idficonic@gmail.com","name":"Iconic Dream Focus"}}';
+         
+        $request = new Request('POST', 'https://api.sendgrid.com/v3/mail/send', $headers, $body);
+       $request1 = new Request('POST', 'https://api.sendgrid.com/v3/mail/send', $headers, $bodyadmin);
+              
+                   
+               try{
+                $promise = $client->send($request);
+            
+                 $promise1 = $client->send($request1);
+                }
+                   catch (RequestException $e){
+                echo $e;}
+            }
+            
+                   ?>
