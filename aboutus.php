2941232d7ccad4e74a0ee3fbee3864c9a090ed04
SR Seetharam <srseetharam1999@gmail.com>
2021-07-08 21:49:40 +0530

    vission mission added

--- aboutus.php
+++ aboutus.php
@@ -13,6 +13,13 @@
 <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/5.4.5/css/swiper.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<style>
+
+.cards-{
+
+margin: 10%;
+margin-bottom: 10%;}
+</style>
 </head>
 <body>
      <!--========== HEADER ==========-->
@@ -29,10 +36,43 @@
       
       <!----------------header------------>
       <div class="container">
+        <div class="row cards-">
+  <div class="col-lg-4 col-md-4 col-12">
+
+    <div class="card" style="border: none;" data-aos="flip-right" data-aos-delay="1000">
+      <img src="./assets/MISSION.png" class="card-img-top" alt="Mission">
+      <div class="card-body">
+        <h2 class="card-text">Future source</h2>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <div class="card" style="border: none;" data-aos="flip-right" data-aos-delay="1100">
+      <img src="./assets/vission.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h2 class="card-text">To cook <span>iconic</span> products to connect the world</h2>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-12">
+    <div class="card" style="border: none;" data-aos="flip-right" data-aos-delay="1200">
+      <img src="./assets/values.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h2 class="card-text">To Connect world and people</h2>
+        
+      </div>
+    </div>
+  </div>
+
+
+        </div>
         <div class="row">
           <div class="col-lg-4 col-md-8">
            
-            <div class = "about-left-content" data-aos="flip-right" data-aos-delay="1100">
+            <div class = "about-left-content" data-aos="flip-right" data-aos-delay="1300">
               <div>
                 <div class = "shadow">
                   <div class = "about-img">
@@ -40,22 +80,22 @@
                   </div>
                 </div>
     
-                <h2 data-aos="zoom-in" data-aos-delay="1300">John Kiran E</h2>
-                <h3 data-aos="zoom-out" data-aos-delay="1500">Founder/CEO</h3>
+                <h2 data-aos="zoom-in" data-aos-delay="1400">John Kiran E</h2>
+                <h3 data-aos="zoom-out" data-aos-delay="1600">Founder/CEO</h3>
               </div>
     
               <ul class = "icons">
-                <li data-aos="flip-right" data-aos-delay="1500"><i class = "fab fa-facebook-f" ></i></li>
-                <li data-aos="flip-right" data-aos-delay="1600"><i class = "fab fa-instagram" ></i></li>
-                <li data-aos="flip-right" data-aos-delay="1700"><i class = "fab fa-twitter" ></i></li>
-                <li data-aos="flip-right" data-aos-delay="1800"><i class = "fab fa-linkedin-in" ></i></li>
+                <li data-aos="flip-right" data-aos-delay="1400"><i class = "fab fa-facebook-f" ></i></li>
+                <li data-aos="flip-right" data-aos-delay="1500"><i class = "fab fa-instagram" ></i></li>
+                <li data-aos="flip-right" data-aos-delay="1600"><i class = "fab fa-twitter" ></i></li>
+                <li data-aos="flip-right" data-aos-delay="1700"><i class = "fab fa-linkedin-in" ></i></li>
               </ul>
             </div>
          
           </div>
 <div class="col-lg-8 col-md-8">
   <div class = "about-right">
-    <h1 data-aos="flip-left"data-aos-delay="1000" >Bonjur<span>!</span></h1>
+    <h1 data-aos="flip-left"data-aos-delay="1000" class="welcome">Hello<span>!</span></h1>
     <h2 data-aos="zoom-in" data-aos-delay="1200">Here's who we are & what we do</h2>
     <div class = "about-btns">
       <a href="team.php" data-aos="fade-left" data-aos-delay="1300"><button type = "button" class = "btn btn-pink">Our Team</button></a>
